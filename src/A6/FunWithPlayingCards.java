0971243958e315541132499c0789598bb1dce84f
Patrick-Rideout <prideout2002@gmail.com>
2023-11-09 14:33:08 -0330

    Update

--- src/A6/FunWithPlayingCards.java
+++ src/A6/FunWithPlayingCards.java
@@ -1,11 +1,17 @@
 package A6;
 
+import java.io.PrintStream;
+
+PrintStream printstream = new PrintStream();
+
 public class FunWithPlayingCards {
 
     public static void main(String[] args) {
         System.out.println("Fun with Playing Cards!");
 
         PlayingCard playingCard = new PlayingCard(2, PlayingCard.Suit.DIAMONDS);
+
+        System.out.println(playingCard.printAsciiCard(playingCard););
 
 
     }
--- src/A6/PlayingCard.java
+++ src/A6/PlayingCard.java
@@ -1,5 +1,6 @@
 package A6;
 
+import java.io.PrintStream;
 public class PlayingCard {
     public static final int ACE = 14;
 
@@ -25,27 +26,39 @@ public class PlayingCard {
     @Override
     public String toString() {
         String valueStr;
-        if (value == 11) valueStr = "J";
-        else if (value == 12) valueStr = "Q";
-        else if (value == 13) valueStr = "K";
-        else if (value == 14) valueStr = "A";
-        else valueStr = String.valueOf(value);
+        if (value == 11) {
+            valueStr = "J";
+        } else if (value == 12) {
+            valueStr = "Q";
+        } else if (value == 13) {
+            valueStr = "K";
+        } else if (value == 14) {
+            valueStr = "A";
+        } else {
+            valueStr = String.valueOf(value);
+        };
 
         String suitSymbol = switch (suit) {
             case DIAMONDS -> "\u2666";
             case HEARTS -> "\u2665";
             case CLUBS -> "\u2663";
             case SPADES -> "\u2660";
-            default -> "?";
         };
 
         return valueStr + " " + suitSymbol;
     }
 
-//    public static void printAsciiCard(PlayingCard playingCard, PrintStream printStream) {
-//        // TODO: Implement this method to print ASCII representation of the card
-//
-//    }
+    public static void printAsciiCard(PlayingCard playingCard, PrintStream printStream) {
+        String suitSymbol = switch (playingCard.getSuit()) {
+            case DIAMONDS -> "\u2666";
+            case HEARTS -> "\u2665";
+            case CLUBS -> "\u2663";
+            case SPADES -> "\u2660";
+        };
+
+        printStream.println(playingCard.getSuit());
+
+    }
 
     public enum Suit {DIAMONDS, HEARTS, CLUBS, SPADES}
 }
